4ab632d463d7eaabf773d5dccd019d310dc1e9e2
MyTuky <362029446+MyTuky@users.noreply.github.com>
2018-01-12 08:24:29 +0800

    调整控件

--- TKSocketServer/ServerForm.Designer.cs
+++ TKSocketServer/ServerForm.Designer.cs
@@ -42,47 +42,53 @@
             // 
             // txtPath
             // 
-            this.txtPath.Location = new System.Drawing.Point(70, 88);
+            this.txtPath.Location = new System.Drawing.Point(52, 303);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(165, 25);
+            this.txtPath.Size = new System.Drawing.Size(273, 21);
             this.txtPath.TabIndex = 0;
             // 
             // txtMsg
             // 
-            this.txtMsg.Location = new System.Drawing.Point(70, 273);
+            this.txtMsg.Location = new System.Drawing.Point(52, 218);
+            this.txtMsg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtMsg.Multiline = true;
             this.txtMsg.Name = "txtMsg";
-            this.txtMsg.Size = new System.Drawing.Size(526, 78);
+            this.txtMsg.Size = new System.Drawing.Size(396, 63);
             this.txtMsg.TabIndex = 0;
             // 
             // txtLog
             // 
-            this.txtLog.Location = new System.Drawing.Point(70, 139);
+            this.txtLog.Location = new System.Drawing.Point(52, 111);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtLog.Multiline = true;
             this.txtLog.Name = "txtLog";
-            this.txtLog.Size = new System.Drawing.Size(562, 105);
+            this.txtLog.Size = new System.Drawing.Size(422, 85);
             this.txtLog.TabIndex = 0;
             // 
             // cboUsers
             // 
             this.cboUsers.FormattingEnabled = true;
-            this.cboUsers.Location = new System.Drawing.Point(70, 45);
+            this.cboUsers.Location = new System.Drawing.Point(52, 36);
+            this.cboUsers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cboUsers.Name = "cboUsers";
-            this.cboUsers.Size = new System.Drawing.Size(165, 23);
+            this.cboUsers.Size = new System.Drawing.Size(125, 20);
             this.cboUsers.TabIndex = 1;
             // 
             // txtPort
             // 
-            this.txtPort.Location = new System.Drawing.Point(70, 379);
+            this.txtPort.Location = new System.Drawing.Point(52, 70);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPort.Name = "txtPort";
-            this.txtPort.Size = new System.Drawing.Size(375, 25);
+            this.txtPort.Size = new System.Drawing.Size(125, 21);
             this.txtPort.TabIndex = 0;
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(265, 42);
+            this.btnStart.Location = new System.Drawing.Point(199, 34);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(93, 71);
+            this.btnStart.Size = new System.Drawing.Size(70, 57);
             this.btnStart.TabIndex = 2;
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -90,9 +96,10 @@
             // 
             // btnSelect
             // 
-            this.btnSelect.Location = new System.Drawing.Point(569, 379);
+            this.btnSelect.Location = new System.Drawing.Point(427, 303);
+            this.btnSelect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSelect.Name = "btnSelect";
-            this.btnSelect.Size = new System.Drawing.Size(93, 26);
+            this.btnSelect.Size = new System.Drawing.Size(70, 21);
             this.btnSelect.TabIndex = 2;
             this.btnSelect.Text = "Select";
             this.btnSelect.UseVisualStyleBackColor = true;
@@ -100,9 +107,10 @@
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(602, 273);
+            this.btnSend.Location = new System.Drawing.Point(452, 218);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(93, 78);
+            this.btnSend.Size = new System.Drawing.Size(70, 62);
             this.btnSend.TabIndex = 2;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -110,9 +118,10 @@
             // 
             // btnSendFile
             // 
-            this.btnSendFile.Location = new System.Drawing.Point(458, 379);
+            this.btnSendFile.Location = new System.Drawing.Point(344, 303);
+            this.btnSendFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSendFile.Name = "btnSendFile";
-            this.btnSendFile.Size = new System.Drawing.Size(93, 26);
+            this.btnSendFile.Size = new System.Drawing.Size(70, 21);
             this.btnSendFile.TabIndex = 2;
             this.btnSendFile.Text = "SendFile";
             this.btnSendFile.UseVisualStyleBackColor = true;
@@ -120,9 +129,10 @@
             // 
             // btnZD
             // 
-            this.btnZD.Location = new System.Drawing.Point(458, 411);
+            this.btnZD.Location = new System.Drawing.Point(344, 329);
+            this.btnZD.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnZD.Name = "btnZD";
-            this.btnZD.Size = new System.Drawing.Size(93, 26);
+            this.btnZD.Size = new System.Drawing.Size(70, 21);
             this.btnZD.TabIndex = 2;
             this.btnZD.Text = "震动";
             this.btnZD.UseVisualStyleBackColor = true;
@@ -130,9 +140,9 @@
             // 
             // ServerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(710, 449);
+            this.ClientSize = new System.Drawing.Size(532, 359);
             this.Controls.Add(this.btnZD);
             this.Controls.Add(this.btnSendFile);
             this.Controls.Add(this.btnSend);
@@ -143,6 +153,7 @@
             this.Controls.Add(this.txtMsg);
             this.Controls.Add(this.txtPort);
             this.Controls.Add(this.txtPath);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ServerForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
